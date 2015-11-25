26b5cbe1cc25bf510e7a795d9f6983e9964250f4
Robin Devouge <robin@MacBook-Pro-de-Robin.local>
2015-11-25 10:06:07 +0100

    Correct mistakes and progess
    - got « news.php » back to original
    - made design for the posts page (private.php)

--- private.php
+++ private.php
@@ -1,16 +1,16 @@
 <?php
 
     // First we execute our common code to connection to the database and start the session
-    require("common.php");
+//    require("common.php");
     
     // At the top of the page we check to see whether the user is logged in or not
     if(empty($_SESSION['user']))
     {
         // If they are not, we redirect them to the login page.
-        header("Location: login.php");
+//        header("Location: login.php");
         
         // Remember that this die statement is absolutely critical.  Without it, people can view your members-only content without logging in.
-        die("Redirecting to login.php");
+//        die("Redirecting to login.php");
     }
     
     // Everything below this point in the file is secured by the login system.
@@ -50,12 +50,16 @@
              R�alis� par Cl�ment SCHMOUKER -->
         <!-- Navigation -->
         <nav>
-            <ul>
-                <li><a class="logo" href="index.php"><img src="_img/logo.svg" alt="logo" /><p>Embryo</p></a></li>
-                    <?php 
+			<ul class="mainNav">
+				<li><a class="navElement logo" href="index.php">Embryo</a></li>
+				<li class="navElement empty"></li>
+				<li><a class="navElement navLink" href="index.php">Accueil</a></li>
+				<li><a href="private.php" class="navElement navLink onPage">Posts</a></li>
+				<?php              
+
                         if ($_SESSION['user']['admin'] == 1) {
-                            echo"<li><a href='admin.php'>Admin</a></li>";
-                        } 
+                            echo"<li><a href='admin.php' class='navElement navLink'>Admin</a></li>";
+                        }        
                         if(empty($_SESSION['user']))
                         {
                                 echo" ";
@@ -63,39 +67,38 @@
                             }
                         else
                         {
-                            echo"<li><a href='logout.php'>Logout</a></li>";
+                            echo"<li><a href='logout.php' class='navElement navLink'>Logout</a></li>";
                         } 
                     ?>
-                <li><a href="edit_account.php">
-                    <?php 
-                        if(empty($_SESSION['user']))
-                        {
-                                echo"Log in";
-                            }
-                        else
-                        {
-                            echo htmlentities($_SESSION['user']['username'], ENT_QUOTES, 'UTF-8');
-                        } 
-                    ?>
-                    </a>
-                </li>
-                <li><a href="index.php">Accueil</a></li>
-                <li><a class="onPage" href="private.php">Posts</a></li>
-            </ul>
-        </nav>
+					<li>
+						<a href="edit_account.php" class="navElement navLink">
+							<?php 
+					if(empty($_SESSION['user']))
+					{
+							echo"Login";
+						}
+					else
+					{
+						echo htmlentities($_SESSION['user']['username'], ENT_QUOTES, 'UTF-8'); //protect script injection via name
+					} 
+				?>
+						</a>
+					</li>
+			</ul>
+		</nav>
 		<!-- Main Content -->
-		<div class="mainContent__Display">
+		<main>
 			<h1>Ce que l'on dit...</h1>
-                        <a href="post.php" class="postButton">
-                <img src="_img/message31.svg" alt="message" />
-                <p>Poster un message</p>
-            </a>
+            <a href="post.php" class="button button--onWhite">Poster un message</a>
 			<?php
 			include("get.php");
 			?>
-		</div>
+			<a href="post.php" class="button button--onWhite">Poster un message</a>
+		</main>
         <!-- JQuery / JS Insertion -->
+<!--
         <script src="_js/jquery-1.11.2.min.js"></script>
         <script src="_js/main.js"></script>
+-->
     </body>
 </html>
